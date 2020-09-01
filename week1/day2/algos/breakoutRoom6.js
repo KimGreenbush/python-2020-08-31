0d9443229d962cc8a86aed0ab92d75add06260f2
Chris Bautista <CBaut@users.noreply.github.com>
2020-09-01 09:38:51 -0700

    algos

---
+++ week1/day2/algos/breakoutRoom6.js
@@ -0,0 +1,76 @@
+// RIOT Walk
+
+// Repeat
+// Input
+// Output
+// Test
+
+// Walk-through
+
+// Breakout room members
+// Fill out members array with name
+
+var members = ["member name"]
+
+// ************************************************
+
+/*
+  String: Reverse
+
+  Implement reverseString(str)
+  Input: 'abc'
+  Output: 'cba'
+*/
+
+function reverseString(str) {
+  // code here
+}
+
+var reverseStringTestCase = "abc"
+var reverseStringReturnValue = reverseString(reverseStringTestCase)
+console.log(reverseStringReturnValue)
+
+// ************************************************
+
+/*
+  Acronyms
+
+  Create a function that, given a string, returns the string’s acronym
+  (first letter of each word capitalized).
+
+  Do it with .split first if you need to, then try to do it without
+
+  input: 'The quick brown fox, jumped over the lazy dog.'
+  output: 'TQBFJOTLD'
+
+  Hint: string.toUpperCase()
+
+*/
+
+function stringAcronym(str) {
+  // code here
+}
+
+var stringAcronymTestCase = "abc"
+var stringAcronymReturnValue = stringAcronym(stringAcronymTestCase)
+console.log(stringAcronymReturnValue)
+
+// ************************************************
+
+/*
+  Case insensitive string comparison
+
+  const test1StrA = "ABC"
+  const test1StrB = "abc"
+  caseInsensitiveCompare(test1StrA, test1StrB) // Output: true
+*/
+
+function caseInsensitiveCompare(str1, str2) {
+  // code here
+}
+
+var caseInsensitiveCompareTestCase = "abc"
+var caseInsensitiveCompareReturnValue = stringAcronym(
+  caseInsensitiveCompareTestCase
+)
+console.log(caseInsensitiveCompareReturnValue)
