20c47a14466770b7ddd14d0fdd3cd81a0d0a14a7
Chris Bautista <CBaut@users.noreply.github.com>
2020-09-02 15:06:14 -0700

    algos

--- week1/day3/algos.js
+++ week1/day3/algos.js
@@ -39,8 +39,8 @@ function join(arr, separator) {
   then the number of times it appears.
 
 
-  If final result is not shorter (such as "bb" => "b2" ),
-  return the original string.
+  // If final result is not shorter (such as "bb" => "b2" ),
+  // return the original string.
   */
 
 // var str1 = "aaaabbcddd"
@@ -49,25 +49,88 @@ function join(arr, separator) {
 // var str2 = ""
 // var expected2 = ""
 
-// var str3 = "a"
-// var expected3 = "a"
+// var str3 = "aa"
+// var expected3 = "a2"
 
 // var str4 = "bbcc"
-// var expected4 = "bbcc"
+// var expected4 = "b2c2"
 
 function encodeStr(str) {
-  // code here
+  // pseudo code
+  // if (str.length < ) {
+  //   return str
+  // }
+
+  // count something
+  var letterCount = 1
+  var newStr = ""
+
+  // loop to read the string
+  for (var index = 0; index < str.length; index++) {
+    console.log(str[index])
+    // compare current value to next value
+    if (str[index] == str[index + 1]) {
+      // if same letterCount++
+      console.log("same value")
+      letterCount++
+      // letterCount += 1
+      // letterCount = letterCount + 1
+    } else {
+      // else str+= letterCount
+      console.log("NOT same value")
+      newStr = newStr + str[index] + letterCount
+      letterCount = 1
+    }
+  }
+
+  // return a new string
+  return newStr
 }
 
+// var returnValue = encodeStr(str3)
+// console.log("returnValue: ", returnValue)
 /* ******************************************************************************** */
 
 /*
   String Decode
 */
 
-// var str1 = "a3b2c1d3"
-// var expected1 = "aaabbcddd"
+// case for max number of 9
+var str1 = "a3b2c1d3"
+var expected1 = "aaabbcddd"
 
 function decodeStr(str) {
   // code here
+  // newStr variable
+  var newStr = ""
+
+  // loop through string
+  for (var i = 0; i < str.length; i += 2) {
+    // step by 2
+    console.log(str[i])
+    // add number of i's by the number of i's +1
+    //                         '3'
+    // newStr += str[i].repeat(str[i + 1])
+    newStr += str[i].repeat(parseInt(str[i + 1]))
+  }
+
+  console.log(newStr)
+  return newStr
+}
+// decodeStr(str1)
+
+// case for number more than 9
+var str2 = "a13 b22 c1 d3"
+var expected2 = "a*13 b*22 c*1 d*3"
+
+function decodeStr2(str) {
+  // var returnStr = ''
+  // var tempStr = ''
+  // for (var i = 0; i < str.length; i++) {
+  //   // if str[i] == Number
+  //   // typeof()
+  //   if () {
+  //     //
+  //   }
+  // }
 }
