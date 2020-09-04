933244c37d26e90c888800cf4711955434c50a29
Chris Bautista <CBaut@users.noreply.github.com>
2020-09-04 16:53:52 -0700

    session

--- week1/day5/form_project/demo_app/urls.py
+++ week1/day5/form_project/demo_app/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index),  # localhost:8000/
     path('login', views.login),
-    # path('dashboard', views.dashboard),
+    path('dashboard', views.dashboard),
+    path('logout', views.logout),
+    path('add_drink', views.add_drink),
 ]
--- week1/day5/form_project/demo_app/views.py
+++ week1/day5/form_project/demo_app/views.py
@@ -3,22 +3,51 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
+    print('Found the index route!')
+    if 'user_name' not in request.session:
+        request.session['user_name'] = 'Anonymous'
+    if 'fav_drink' not in request.session:
+        request.session['fav_drink'] = []
     return render(request, 'index.html')
 
 
 def login(request):
-    print(request.POST)
+    print('request.POST: ', request.POST)
     # POST Request
-    print('Logging in the user')
     # do some db stuff
-    name = request.POST['user_namez']
-    print(f'The username is {name}')
-    context = {
-      'name_to_use_on_template': name
-    }
-    return render(request, 'dashboard.html', context)
-    # return redirect('/dashboard')
+
+    # name = request.POST['user_namez']
+    # request.session['user_name'] = name
+    # print(f'The username is {name}')
+
+    print('Logging in the user by adding their name to the session dictionary')
+    request.session['user_name'] = request.POST['user_namez']
+    # request.session['fav_drink'] = request.POST['fav_drink']
+    request.session['fav_drink'].append(request.POST['fav_drink'])
+    request.session.save()
+
+    print('Redirecting to the dashboard!')
+    return redirect('/dashboard')
 
 
-# def dashboard(request):
-#     return render(request, 'dashboard.html')
+def dashboard(request):
+    # if 'user_name' not in request.session:
+    #     return redirect('/')
+
+    print('Rendering the dashboard!')
+    return render(request, 'dashboard.html')
+
+
+def logout(request):
+    print('Logging a user out')
+    # del request.session['user_name']
+    # del request.session['fav_drink']
+    request.session.flush()
+    return redirect('/')
+
+
+def add_drink(request):
+    print(request.POST)
+    request.session['fav_drink'].append(request.POST['new_drink'])
+    request.session.save()
+    return redirect('/dashboard')
