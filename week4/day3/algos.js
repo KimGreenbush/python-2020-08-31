93f5eb5510df1a4da24839faf3a3de2eb0b9eccc
Chris Bautista <CBaut@users.noreply.github.com>
2020-09-23 15:30:29 -0700

    solutions

--- week4/day3/algos.js
+++ week4/day3/algos.js
@@ -12,7 +12,13 @@
 // const str2 = ""
 // const expected2 = ""
 
-function reverseStr(str) {}
+function reverseStr2(str, i = str.length - 1) {
+  if (i === 0) {
+    return str[i]
+  } else {
+    return str[i] + reverseStr2(str, i - 1)
+  }
+}
 
 /* ******************************************************************************** */
 
@@ -29,17 +35,38 @@ function reverseStr(str) {}
   Based on that comparison, narrow your search to a particular section of the array
 */
 
-// const nums1 = [1, 3, 5, 6];
-// const searchNum1 = 4;
-// const expected1 = false;
+// const nums1 = [1, 3, 5, 6]
+// const searchNum1 = 4
+// const expected1 = false
 
-// const nums2 = [4, 5, 6, 8, 12];
-// const searchNum2 = 5;
-// const expected2 = true;
+// const nums2 = [4, 5, 6, 8, 12]
+// const searchNum2 = 5
+// const expected2 = true
 
-// const nums3 = [3, 4, 6, 8, 12];
-// const searchNum3 = 3;
-// const expected3 = true;
+// const nums3 = [3, 4, 6, 8, 12]
+// const searchNum3 = 3
+// const expected3 = true
 
 // add params if needed for recursion
-function binarySearch(sortedNums, searchNum) {}
+function binarySearch(
+  sortedNums,
+  searchNum,
+  leftIdx = 0,
+  rightIdx = sortedNums.length - 1
+) {
+  if (leftIdx > rightIdx) {
+    return false
+  }
+
+  const midIdx = Math.floor((rightIdx + leftIdx) / 2)
+
+  if (sortedNums[midIdx] === searchNum) {
+    return true
+  }
+
+  if (searchNum < sortedNums[midIdx]) {
+    return binarySearch(sortedNums, searchNum, 0, midIdx - 1)
+  } else {
+    return binarySearch(sortedNums, searchNum, midIdx + 1, rightIdx)
+  }
+}
