6c4955a00213ea98e1526e1e4fd4df918bfb1ba7
Chris Bautista <CBaut@users.noreply.github.com>
2020-09-24 10:47:27 -0700

    solution

--- week4/day4/algos.js
+++ week4/day4/algos.js
@@ -23,4 +23,26 @@ const expected3 = 7
 const num4 = 156 // 1+5+6 = 12 -> 1+2 = 3
 const expected4 = 3
 
-function sumToOneDigit(num) {}
+function sumToOneDigit(n) {
+  console.log(n)
+  // Termination Condition if it's bad data (not a number)
+  if (isNaN(parseInt(n))) {
+    return null
+  }
+
+  // base case: return solution
+  if (n < 10) {
+    return n
+  }
+
+  const strNum = n.toString()
+  let sum = 0
+
+  for (const strDigit of strNum) {
+    sum += parseInt(strDigit)
+  }
+
+  return sumToOneDigit(sum)
+}
+
+console.log(sumToOneDigit(num4))
